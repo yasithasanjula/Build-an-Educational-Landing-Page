f3b8b7e0ab983efffae1430f29e5f583e698c64f
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-22 01:03:18 +0530

    11

--- Edu-Web/src/Components/Navbar.jsx
+++ Edu-Web/src/Components/Navbar.jsx
@@ -4,26 +4,26 @@ import {Link} from'react-scroll'
 const Navbar = () => {
   return (
     <div>
-        <div>
+        <div className='flex flex-row justify-between p-5 md:px-32 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
             <div>
-                <Link to='/'>
+                <Link to='/' className='font-semibold text-2xl p-1 cursor-pointer'>
                     eStudy
                 </Link>
             </div>
-           <nav>
-                <Link to='home' spy={true} smooth={true} duration={500}>
+           <nav className='hidden md:flex gap-5 font-medium p-1 cursor-pointer'>
+                <Link to='home' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
                     Home
                 </Link>
-                <Link to='about' spy={true} smooth={true} duration={500}>
+                <Link to='about' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
                     About
                 </Link>
-                <Link to='courses' spy={true} smooth={true} duration={500}>
+                <Link to='courses' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
                     Courses
                 </Link>
-                <Link to='reviews' spy={true} smooth={true} duration={500}>
+                <Link to='reviews' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
                     Reviews
                 </Link>
-                <Link to='contact' spy={true} smooth={true} duration={500}>
+                <Link to='contact' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
                     Contact
                 </Link>
            </nav>
