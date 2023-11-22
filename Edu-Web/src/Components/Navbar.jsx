cf6b3f52192ea2c39fb8da630d5c4a349742b7b7
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-22 09:15:00 +0530

    1111

--- Edu-Web/src/Components/Navbar.jsx
+++ Edu-Web/src/Components/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from'react-scroll'
+import {AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
+    const [menu,setMenu] = useState(false);
+    const handleChange = () => {
+        setMenu(!menu);
+    };
+
   return (
     <div>
         <div className='flex flex-row justify-between p-5 md:px-32 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
@@ -27,6 +33,31 @@ const Navbar = () => {
                     Contact
                 </Link>
            </nav>
+             <div className='flex md:hidden' onClick={handleChange}>
+                <div className='p-2'>
+                    <AiOutlineMenu size={22}/>
+                </div>
+             </div>
+        </div>
+
+        <div className={`${
+            menu ? 'tramslate-x-0' : '-translate-x-full'
+        } md:hidden flex flex-col absolute bg-white left-0 top-20 font-medium text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}>
+                <Link to='home' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
+                    Home
+                </Link>
+                <Link to='about' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
+                    About
+                </Link>
+                <Link to='courses' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
+                    Courses
+                </Link>
+                <Link to='reviews' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
+                    Reviews
+                </Link>
+                <Link to='contact' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
+                    Contact
+                </Link>
         </div>
     </div>
   )
