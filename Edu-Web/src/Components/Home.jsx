3a8dfd98c1f3ef9f7e6726e2cbbc0532cad82eda
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-22 09:36:54 +0530

    111

--- Edu-Web/src/Components/Home.jsx
+++ Edu-Web/src/Components/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import  Button  from '../layout/Button'
+import {Link} from 'react-scroll'
 
 const Home = () => {
   return (
     <div>
         <div>
-            <h2>Knowladge</h2>
+            <h2>Knowledge with 
+                <span>eStudy</span>
+            </h2>
+            <p>
+            eStudy is your gateway to a world of limitless learning possibilities.
+          With our cutting-edge eLearning platform, you can explore a vast
+          library of courses, from academic subjects to practical skills, all
+          designed to help you achieve your goals.
+            </p>
+
+            <Link to='contact' spy={true} smooth={true} duration={500}>
+                <Button/>
+            </Link>
         </div>
     </div>
   )
