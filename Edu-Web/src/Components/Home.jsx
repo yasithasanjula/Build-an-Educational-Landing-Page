f90094df19bc2f340337ac828f7ab6c0a48355e2
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-22 09:43:15 +0530

    111

--- Edu-Web/src/Components/Home.jsx
+++ Edu-Web/src/Components/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import  Button  from '../layout/Button'
 import {Link} from 'react-scroll'
+import img from '../assets/hero.svg'
 
 const Home = () => {
   return (
-    <div>
-        <div>
+    <div className='min-h-[70vh] flex flex-col md:flex-row md:justify-between items-center md:mx-32 mx-5 mt-10'>
+        <div className='md:w-2/4 text-center'>
             <h2>Knowledge with 
                 <span>eStudy</span>
             </h2>
@@ -17,8 +18,11 @@ const Home = () => {
             </p>
 
             <Link to='contact' spy={true} smooth={true} duration={500}>
-                <Button/>
+                <Button title='Contact Us'/>
             </Link>
+        </div>
+        <div>
+           <img src={img} alt="img" /> 
         </div>
     </div>
   )
