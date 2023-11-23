89c2d99017389e7cd12495a7bc556111b78a8ce0
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-23 08:08:16 +0530

    111

--- Edu-Web/src/Components/Footer.jsx
+++ Edu-Web/src/Components/Footer.jsx
@@ -1,13 +1,44 @@
 import React from 'react'
+import {Link} from 'react-scroll'
 
 const Footer = () => {
   return (
     <div>
-        <div>
-            <div>
-                
-            </div>
+        <div className='flex flex-col md:flex-row justify-between p-5 bg-white border-2 border-lightText rounded-lg md:px-32 px-5'>
+          <div className='flex flex-col md:flex-row gap-5 font-medium p-1 text-lg'>            
+            
+                <Link to='home' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
+                    Home
+                </Link>
+                <Link to='about' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
+                    About
+                </Link>
+                <Link to='courses' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
+                    Courses
+                </Link>
+                <Link to='reviews' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
+                    Reviews
+                </Link>
+                <Link to='contact' spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>
+                    Contact
+                </Link> 
+          </div>
+
+          <div>
+              <Link to='/' className='font-semibold text-2xl p-1 cursor-pointer'>
+              eStudy
+              </Link>
+          </div>
+        
         </div>
+
+        <div className='text-center mt-4'>
+            <p>@copyright developed by
+              <span className='text-brightGreen px-2'> YS programmers</span>
+              All rights reserved
+            </p>
+          </div>
+          
     </div>
   )
 }
